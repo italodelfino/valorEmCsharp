f971305ffb84f0190ed035626c061c28e3530db1
italodelfino <italo_martins@outlook.com>
2022-05-12 09:59:43 -0300

    atualizacao 02

---
+++ Pessoa.cs
@@ -0,0 +1,8 @@
+public class Name
+{
+   public int Idade { get; set; }
+
+   public string Nome { get; set; }
+
+   public string Documento { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -2,10 +2,35 @@
 using static System.Console;
 public class Program
 {
+    static void Demo1()
+    {
+        int a = 2;
+        Adicionar20(a);
+        WriteLine($"O valor da variável a é: {a}");
+    }
+    static void Adicionar20(int a)
+    {
+        a = a + 20;
+    }
+    
+    static void trocarNome(Pessoa p1, string nomeNovo)
+    {
+        p1.Nome = nomeNovo;
+    }
     public static void Main()
     {
-        WriteLine("Hello Word");
+        Pessoa p1 = new Pessoa();
+        p1.Nome = "Ricardo";
+        p1.Idade = 30;
+        p1.Documento ="1234";
+
+        trocarNome(p1, "José");
+
+        WriteLine($"O novo Nome é : {p1.Nome}");
     }
+
+
 }
+
 
 
